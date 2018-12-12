4f257eb3d10688d7283ec078842c2ea527fb6b0f
Ron Pechuk <rpechuk@gmail.com>
2018-12-11 19:04:26 -0800

    Major changes with table and div

--- brain/main.jsx
+++ brain/main.jsx
@@ -221,36 +221,46 @@ const Year = ({
       {message}
     </li>)
   }</ul>
-  <table class="table table-borderless-xl table-dark">
-    <tbody>
-      <tr>
-        <th>
-          <h2>Resources</h2>
-          <h3>Gold: {value.gold}
-          </h3>
-          <h3>Taxes per farmer: {value.taxes} gold</h3>
-          <button className="btn btn-primary" onClick={onFarmerToTaxer}>Add Taxes</button>
-          <h3>Wheat in storage: {value.wheat}</h3>
-          <button className="btn btn-success" onClick={onFieldToStorage}>&uarr;</button>
-          <button className="btn btn-danger" onClick={onStorageToField}>&darr;</button>
-          <h3>Field: {value.saw}</h3>
-        </th>
-        <th>
-          <h2>People</h2>
-          <h3>Farmers: {value.farmers}
-          </h3>
-          <button className="btn btn-success" onClick={onLessArmy}>&uarr;</button>
-          <button className="btn btn-danger" onClick={onMoreArmy}>&darr;</button>
-          <h3>Army: {value.army} people</h3>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-        </th>
-      </tr>
-    </tbody>
-  </table>
+  <h3>Gold: {value.gold} </h3>
+  <h3>Taxes per farmer: {value.taxes} gold</h3>
+  <button className="btn btn-success" onClick={onFarmerToTaxer}>&uarr;</button>
+  <button className="btn btn-danger" onClick={onTaxerToFarmer}>&darr;</button>
+
+  <div className="row">
+    <div className="col-sm-9">
+      <h2>Wheat:</h2>
+      <div className="row">
+        <div className="col-8 col-sm-2">
+          <h4>Storage: <br></br> {value.wheat} wheat</h4>
+        </div>
+        <div className="col-8 col-sm-2">
+          <div class="col-9"><button className="btn btn-success btn-block" onClick={onFieldToStorage}>&larr;</button></div>
+          <div class="col-9"><button className="btn btn-danger btn-block" onClick={onStorageToField}>&rarr;</button></div>
+        </div>
+        <div className="col-4 col-sm-8">
+          <h4>Field: {value.saw} <br></br> wheat</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="row">
+    <div className="col-sm-9">
+      <h2>People:</h2>
+      <div className="row">
+        <div className="col-8 col-sm-2">
+          <h4>Farmers: <br></br> {value.farmers} people</h4>
+        </div>
+        <div className="col-8 col-sm-2">
+          <div class="col-9"><button className="btn btn-success btn-block" onClick={onLessArmy}>&larr;</button></div>
+          <div class="col-9"><button className="btn btn-danger btn-block" onClick={onMoreArmy}>&rarr;</button></div>
+        </div>
+        <div className="col-4 col-sm-8">
+          <h4>Army: <br></br>{value.army} people</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>)
 
 const render = () => {
